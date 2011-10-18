3af5e94889c1f7d6164fcc458a077afae19fff5c
Zete Lui <ns@theplant.jp>
2011-10-18 11:16:26 +0800

    renew

--- renew.rb
+++ renew.rb
@@ -1,10 +1,13 @@
 require "fileutils"
+require "open-uri"
 include FileUtils
 
 cd File.dirname(__FILE__) + '/root'
 Dir.glob 'spec*gz' do |f|
-  done = `wget -t 1 http://rubygems.org/#{f} -O ../tmp/#{f} && echo done`
-  if done.strip.end_with?('done')
-    mv "../tmp/#{f}", f
+  data = open "http://rubygems.org/#{f}", &:read rescue nil
+  if data and !data.empty?
+    File.open f, 'w' do |s|
+      s << data
+    end
   end
 end
